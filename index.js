a78bc70f216848aaab34447d8ba85aad378e7222
hamdi Git <hamdihamza440@gmail.com>
2025-01-01 00:11:50 +0100

    chnage index

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express=require('express')
 const app=express()
 
 app.get('/',(req,res)=>{
-    res.send('hello world')
+    res.send('Hello World')
 })
 
-app.listen(3003,()=>{
+const port=process.env.PORT || 3003
+
+app.listen(port,()=>{
     console.log('Server is running on port 3000')
 })
